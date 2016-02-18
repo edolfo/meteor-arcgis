29d3d1a76d7733e7c1ba56bccdad6b8a3b63412b
edolfo <edolfo@hckrlabs.com>
2016-02-18 02:02:04 +0000

    Add/remove functions

--- app/client/todo/directive.import.js
+++ app/client/todo/directive.import.js
@@ -2,17 +2,26 @@
 import { todoModule } from './moduleDefiner';
 
 let todoModuleDirective = todoModule.directive('taskList', ['todoApp.db.tasks',
-    function(db_tasks) {
+    function(db) {
         return {
             restrict: 'E',
             templateUrl: 'client/todo/task-list.html',
             controllerAs: 'taskList',
             controller: function($scope, $reactive) { // Gives us trouble with ng-strict-di
                 $reactive(this).attach($scope);
-
+                $scope.newTask = '';
                 this.helpers({
-                    tasks: () => { return db_tasks.tasks; }
+                    tasks: () => { return db.tasks(); }
                 });
+
+                this.addTask = () => {
+                    db.insert(this.newTask);
+                    this.newTask = '';
+                };
+
+                this.removeTask = (task) => {
+                    db.remove(task);
+                };
             }
         };
     }
--- app/client/todo/service.import.js
+++ app/client/todo/service.import.js
@@ -2,7 +2,18 @@
 
 import { todoModule } from './moduleDefiner';
 let todoModuleService = todoModule.service('todoApp.db.tasks', function(){
+    function tasks() {
+        return Tasks.find({});
+    }
+    function insert(task) {
+        return Tasks.insert({text: task});
+    }
+    function remove(task) {
+        return Tasks.remove({_id: task._id});
+    }
     return {
-        tasks: Tasks.find({})
+        tasks: tasks,
+        insert: insert,
+        remove: remove
     };
 });
